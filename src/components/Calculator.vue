c43fdd4ff125a47904b7aeed47afce766a8162f2
unknown <amilhasbala@gmail.com>
2020-12-27 20:37:33 +0700

    number and computed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,22 +5,32 @@
       <v-col cols="12">
         <v-text-field
           label="Insert current"
+          v-model="initial"
           :rules="rules"
           hide-details="auto"
         ></v-text-field>
+
+        <h1>by the end of on year : {{ result }}</h1>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      rules: [
-        value => !!value || 'Required.',
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    rules: [
+      (value) => !!value || "Required.",
+      (value) => Number(value) || "Must be a Number",
+    ],
+    initial: null,
+  }),
+  computed: {
+    result: function () {
+      return Number(this.initial) + 5;
+    },
+  },
+};
 </script>
 
 <style>
